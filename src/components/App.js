f7f76cca3dd69549d4be81a4cde4d22a67a8219d
Kelvin Brito <britok30@gmail.com>
2019-11-19 16:04:43 -0500

    added all routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import Navbar from "./Navbar";
 import SideBar from "./SideBar";
 import Articles from "./TopArticles/Articles";
 import SearchBar from "./Search/SearchBar";
+import Business from "./Business/Business";
+import Tech from "./Tech/Tech";
+import Sports from "./Sports/Sports";
+import Science from "./Science/Science";
+import Health from "./HealtH/Health";
 import Footer from "./Footer";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -23,6 +28,11 @@ class App extends Component {
                 <Switch>
                   <Route exact path="/" component={Articles} />
                   <Route exact path="/search" component={SearchBar} />
+                  <Route exact path="/business" component={Business} />
+                  <Route exact path="/health" component={Health} />
+                  <Route exact path="/tech" component={Tech} />
+                  <Route exact path="/sports" component={Sports} />
+                  <Route exact path="/science" component={Science} />
                 </Switch>
               </div>
               <Footer />
