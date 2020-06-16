94f7610e335c11cc752267612fa823aab3dcd1a6
Kelvin Brito <britok30@gmail.com>
2020-06-16 02:47:51 -0400

    changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static(path_join(__dirname, "../client/build")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
