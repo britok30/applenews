eebec31a6d9bbcb2f9aadd39cfe3c26cd26c9277
Kelvin Brito <britok30@gmail.com>
2020-06-17 00:24:03 -0400

    express

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var request = require("request");
 const path = require("path");
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path_join(__dirname, "client/build")));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
